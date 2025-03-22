1ef3f2b603632ad46d6bc82b62f3f4c8453e9340
adieb <mrizqiamiruladieb@gmail.com>
2025-03-22 15:32:23 +0700

    feat: panels & FlowLayout

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.awt.FlowLayout;
+import java.awt.*;
 import javax.swing.*;
 
 public class Main {
@@ -16,19 +16,35 @@ public class Main {
         frame.setSize(500, 500);
         frame.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(100, 250));
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(new FlowLayout());
+        
 //        JButton button1 = new JButton("1");
         
 //        frame.add(button1);
-        frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton("3"));
-        frame.add(new JButton("4"));
-        frame.add(new JButton("5"));
-        frame.add(new JButton("6"));
-        frame.add(new JButton("7"));
-        frame.add(new JButton("8"));
-        frame.add(new JButton("9"));
+//        frame.add(new JButton("1"));
+//        frame.add(new JButton("2"));
+//        frame.add(new JButton("3"));
+//        frame.add(new JButton("4"));
+//        frame.add(new JButton("5"));
+//        frame.add(new JButton("6"));
+//        frame.add(new JButton("7"));
+//        frame.add(new JButton("8"));
+//        frame.add(new JButton("9"));
         
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+        
+        frame.add(panel);
         frame.setVisible(true);
         
     }
